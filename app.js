84ec4df9d16e3e448056876ee90bde907b16142c
PranoySarker <pranoysarker01@gmail.com>
2022-02-17 03:06:31 +0600

    responsive

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('connected');
+function validateInputs() {
+    const incomeField = document.getElementById('income-field');
+    const incomeAmount = parseInt(incomeField.value);
+    if (isNaN(incomeAmount) || incomeAmount < 0) {
+        alert('Please enter valid inputs!!')
+    }
+    else {
+        return incomeAmount;
+    }
+}
+
+
+
+document.getElementById('calc-btn').addEventListener('click', function () {
+    // const incomeField = document.getElementById('income-field');
+    // const incomeAmount = parseInt(incomeField.value);
+    // if (isNaN(incomeAmount) || incomeAmount < 0) {
+    //     console.log('it is a string');
+    // }
+    // else {
+    //     console.log(incomeAmount);
+    // }
+    const incomeAmount = validateInputs();
+    console.log(incomeAmount);
+})

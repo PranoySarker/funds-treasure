8b02bc80b01b152d240f27d1dcdb71d4a24c4bce
PranoySarker <pranoysarker01@gmail.com>
2022-02-17 12:55:07 +0600

    above save

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-function validateInputs() {
-    const incomeField = document.getElementById('income-field');
-    const incomeAmount = parseInt(incomeField.value);
-    if (isNaN(incomeAmount) || incomeAmount < 0) {
-        alert('Please enter valid inputs!!')
+function validateInputs(input) {
+    const inputField = document.getElementById(input + '-field');
+    const inputAmount = parseInt(inputField.value);
+    if (isNaN(inputAmount) || inputAmount < 0) {
+        inputField.style.borderBlockColor = 'red';
+        alert('Require valid inputs in ' + input + ' field');
     }
     else {
-        return incomeAmount;
+        inputField.style.borderBlockColor = 'black';
+        return inputAmount;
+
     }
 }
 
 
-
 document.getElementById('calc-btn').addEventListener('click', function () {
-    // const incomeField = document.getElementById('income-field');
-    // const incomeAmount = parseInt(incomeField.value);
-    // if (isNaN(incomeAmount) || incomeAmount < 0) {
-    //     console.log('it is a string');
-    // }
-    // else {
-    //     console.log(incomeAmount);
-    // }
-    const incomeAmount = validateInputs();
+
+    const incomeAmount = validateInputs('income');
     console.log(incomeAmount);
+
+    const foodAmount = validateInputs('food');
+    console.log(foodAmount);
+
+    const rentAmount = validateInputs('rent')
+    console.log(rentAmount);
+
+    const clothesAmount = validateInputs('clothes')
+    console.log(clothesAmount);
+
+    const totalExpense = foodAmount + rentAmount + clothesAmount;
+    console.log(totalExpense);
+    const totalExpenseText = document.getElementById('total-expense')
+    if (totalExpense > incomeAmount) {
+        totalExpenseText.textContent = "Expense should not larger than your Income"
+        totalExpenseText.style.color = "red"
+    }
+    else {
+        totalExpenseText.innerText = totalExpense;
+    }
+    const balance = document.getElementById('balance');
+    balance.innerText = incomeAmount - totalExpense;
 })
+
